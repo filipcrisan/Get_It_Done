8f6433aeed50e0e7fd53f5a81d4afa31741fdfad
Filip Crisan <filcrisan@gmail.com>
2021-02-19 11:26:22 +0200

    refactor

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
-from flask import Flask 
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate 
+from flask_migrate import Migrate
+from models import User
+from auth import auth as auth_bp
+from main import main as main_bp
+import os
 
-
-db = SQLAlchemy() 
+db = SQLAlchemy()
 migrate = Migrate()
-
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = b'9\xe0_H\x03\xb1\x10TVR\xfd\x19\xda\xab\xa2\\\xe8`\xd4\xde\xa3Z\x12\xc9' 
+app.config['SECRET_KEY'] = b'9\xe0_H\x03\xb1\x10TVR\xfd\x19\xda\xab\xa2\\\xe8`\xd4\xde\xa3Z\x12\xc9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app) 
+db.init_app(app)
 migrate.init_app(app, db)
 
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
 login_manager.init_app(app)
 
-from models import User
 
 @login_manager.user_loader
 def load_user(user_id):
-	return User.query.get(int(user_id))
+    return User.query.get(int(user_id))
 
-from auth import auth as auth_bp
+
 app.register_blueprint(auth_bp)
-
-from main import main as main_bp
 app.register_blueprint(main_bp)
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
